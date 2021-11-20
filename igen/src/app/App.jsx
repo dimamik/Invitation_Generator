c1b5f8a2023e7575fc180a3c7dd16d6c1b46ba07
Tomek <tomasz@capchase.com>
2021-11-20 14:16:02 +0100

    add home page

--- igen/src/app/App.jsx
+++ igen/src/app/App.jsx
@@ -1,22 +1,22 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Success from '../pages/success';
-import {GlobalStyles} from "../styles/GlobalStyles";
+import { GlobalStyles } from '../styles/GlobalStyles';
+import Home from '../pages/home';
 
 function App() {
-    return (
-        <>
-        <GlobalStyles />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<>Hello from /</>}/>
-                    <Route path="/form" element={<>Hello from /form</>}/>
-                    <Route path="/success"  element = {<Success />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-
-    );
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<>Hello from /form</>} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- igen/src/pages/home.jsx
+++ igen/src/pages/home.jsx
@@ -1,12 +1,45 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Div = styled.div`
+
+const Container = styled.div`
+  height: 80%;
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
+  align-items: center;
+  font-family: Ubuntu, Roboto, sans-serif;
+  font-size: 64px;
+`;
+
+const Button = styled.div`
+  background-color: indigo;
+  border-radius: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 600px;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Title = styled.h1`
+  font-weight: 600;
+  font-size: 132px;
 `;
 
 const Home = () => {
-    return <>Hello from Home</>
-}
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Title>Invitation Generator</Title>
+      <Button onClick={() => navigate('/form')}>Generate! ➡</Button>
+    </Container>
+  );
+};
 
 export default Home;
