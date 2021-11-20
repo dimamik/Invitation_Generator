077b9eea1715fa610b482e8900132522ff3d2fc9
dimamik <57190429+dimamik@users.noreply.github.com>
2021-11-20 13:12:42 +0100

    Fix UI Configuration

---
+++ igen/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Home = () => {
+    return <>Hello from Home</>
+}
+
+export default Home;
